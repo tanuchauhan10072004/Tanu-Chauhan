9d98e62e29cf0a04a666c07dd2f0b20c703d0b29
tanuchauhan10072004 <tanuchauhan10072004@gmail.com>
2026-05-07 23:52:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Tanu-Chauhan
-A Currency Converter project developed using JavaScript that converts currencies in real time using exchange rate APIs. Users can select different currencies, enter an amount, and instantly get the converted value through a simple, responsive, and interactive web-based interface.
+# Currency Converter
+## This is a simple web application that allows users to convert between different currencies using real-time exchange rates. The project is built with HTML, CSS, and JavaScript.
+
+## Features
+* Convertion between multiple currencies.
+* Fetch real-time exchange rates by using external API used.
+* User-friendly interface.
+* Responsive design that works on both desktop and mobile devices.
+* Flag is visible for both the countries of which the currency is converted
+
+## Technologies Used
+HTML: For the structure of the converter application.
+
+CSS: For styling the user interface and layout.
+
+JavaScript: For the conversion logic and interaction with the exchange rates API or in short brain of the currency converter.
+
+## API Integration
+The application uses the ExchangeRate-API for fetching real-time exchange rates.
+
+## How to use it :
+Convert from USD to EUR:
+
+Select "USD" as the source currency.
+
+Select "EUR" as the target currency.
+
+Enter an amount, e.g., 100.
+
+Click "Convert."
+
+Then application will show the equivalent amount in EUR as a result.
+
---
+++ script.js
@@ -0,0 +1,139 @@
+ /*const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});*/
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const bodyel=document.querySelector("body");
+bodyel.addEventListener("mousemove",(event)=>{
+    const xpos=event.offsetX;
+    const ypos=event.offsetY;
+    const spane1=document.createElement("span");
+    spane1.style.left=xpos+"px";
+    spane1.style.top=ypos+"px";
+    const size=Math.random()*100;
+    spane1.style.width=size+"px";
+    spane1.style.height=size+"px";
+    bodyel.appendChild(spane1);
+   
+   setTimeout(()=>{
+    spane1.remove();
+   },5000);
+});
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.min.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  // console.log(data);
+  let from = fromCurr.value.toLowerCase();
+  // console.log(from);
+  let to = toCurr.value.toLowerCase();
+  // console.log(to);
+
+  let rate = data[from][to];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
